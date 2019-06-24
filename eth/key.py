e0b9f709aea5b6969a9fdeed2db6b01714195a07
bluesky0125 <time2go@yeah.net>
2019-06-24 14:34:13 +0800

    check

--- eth/key.py
+++ eth/key.py
@@ -3,7 +3,17 @@ $ python utils.py UTC--2019-03-14T08-21-57.582333053Z--a69cbac58e16f007cb553a596
 c9cd37e7d47825219002a06bdb2debcb73d89c15b6a50c1f332d4985a62b2610
 """
 
-def get_privkey(filename,passwd):
+from ethereum.utils import sha3
+from ethereum.utils import privtoaddr,privtopub
+from rlp.utils import decode_hex,encode_hex
+
+def privkeyfromstring(string='dog'):
+    privkey = sha3(string)
+    pubkey = privtopub(privkey)
+    addr = privtoaddr(privkey)
+    return encode_hex(addr),encode_hex(privkey),pubkey
+
+def privkeyfromfile(filename,passwd):
     with open(filename) as f:
         import json
         data = json.load(f)
@@ -14,8 +24,9 @@ def get_privkey(filename,passwd):
 def test_get_privkey():
     import sys
     if len(sys.argv) > 2:
-        privkey = get_privkey(sys.argv[1],sys.argv[2])
+        privkey = privkeyfromfile(sys.argv[1],sys.argv[2])
         print(privkey)
-        from ethereum.utils import privtoaddr
-        from rlp.utils import decode_hex
         print(privtoaddr(decode_hex(privkey)))
+        
+if __name__ == "__main__":
+    print(privkeyfromstring())
